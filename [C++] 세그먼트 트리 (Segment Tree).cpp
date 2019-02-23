8707eb7e3b439ff788e27792bdabcbe4c2f7cde1
양창엽 <ChangYeop-Yang@users.noreply.github.com>
2019-02-23 20:26:31 +0900

    http://blog.naver.com/yeop9657/220899551490
    http://blog.naver.com/yeop9657/220899551490

--- [C++] 세그먼트 트리 (Segment Tree).cpp
+++ [C++] 세그먼트 트리 (Segment Tree).cpp
@@ -1,71 +1,54 @@
-#include <algorithm>
 #include <vector>
 #include <cstdio>
+#include <climits>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-#define INF 999999999
+#define INT_PAIR pair<int, int>
 
-/* Struct */
-typedef struct rmq /* 배열의 구간 최소 쿼리를 해결하기 위한 구간 트리 */
-{
-	int n; /* 배열의 길이 */
-	vector<int> rangeMin; /* 각 구간의 최소치 */
-
-	rmq(const vector<int> & array)
-	{
-		n = array.size();
-		rangeMin.resize(n * 4);
-		init(array, 0, n - 1, 1);
+/* Segment Tree: 저장 된 자료들을 적절히 전처리하여 그들에 대한 질의들을 빠르게 대답할 수 있는 트리 */
+typedef struct RMQ {
+	
+	int length = 0; // 배열의 길이를 저장하는 변수
+	
+	vector<int> rangeMin; // 각 구간의 최소치를 저장하는 배열
+	
+	RMQ(const vector<int> & arr) {
+		this->length = arr.size();
+		this->rangeMin.resize(this->length * 4);
+		this->init(arr, 0, length - 1, 1);
 	}
 
-	/* Node가 array[left..right] 배열을 표현할 때 Node를 루트로 하는 서브트리를 초기화하고, 이 구간의 최소치를 반환한다. */
-	int init(const vector<int> & array, int left, int right, int node)
-	{
-		/* 터미널 노드일 경우 */
-		if (left == right) { return rangeMin[node] = array[left]; }
+	// Node 노드가 Array[left..right] 배열을 표현할 때, Node를 루트로 하는 서브트리를 초기화하고, 이 구간의 최소치를 반환한다.
+	const int init(const vector<int> & arr, int left, int right, int node) {
+		/* 현재 구간을 두 개로 나누어 재귀 호출한 뒤, 두 구간의 최소치 중 더 작은 값을 선택하여 해당 구간의 최소치를 계산한다. */
+	
+		/* 터미널 노드 (Leaf node, a node of a tree data structure that has no child nodes.)일 경우 */
+		if (left == right) { return (this->rangeMin[node] = arr[left]); }
+		
+		const int mid = (left + right) / 2;
+		const int leftMin = init(arr, left, mid, node * 2);
+		const int rightMin = init(arr, mid + 1, right, node * 2 + 1);
 
-		int mid = (left + right) / 2;
-		int leftMin = init(array, left, mid, node * 2);
-		int rightMin = init(array, mid + 1, right, node * 2 + 1);
-		return rangeMin[node] = min(leftMin, rightMin);
+		return this->rangeMin[node] = min(leftMin, rightMin); // Time Complxity - log(n)
 	}
 
-	/* Node가 표현하는 범위 array[nodeLeft..nodeRight]가 주어질 때 이 범위와 array[left..right]의 교집합의 최소치를 구한다. */
-	int query(int left, int right, int node, int nodeLeft, int nodeRight)
-	{
-		/* 두 구간이 겹치지 않으면 아주 큰 값을 반환한다. : 무시되는 경우 */
-		if (right < nodeLeft || nodeRight < left) { return INF; }
-		/* Node가 표현하는 범위가 array[left..right]에 완전히 포함되는 경우 */
-		if (left <= nodeLeft && nodeRight <= right) { return rangeMin[node]; }
-		/* 양쪽 구간을 나눠서 푼 뒤 결과를 합친다. */
-		int mid = (nodeLeft + nodeRight) / 2;
-		return min(query(left, right, node * 2, nodeLeft, mid), query(left, right, node * 2 + 1, mid + 1, nodeRight));
-	}
+	private :
+		// Node가 표현하는 범위 [NodeLeft, NodeRight]와 우리가 최소치를 찾기 원하는 구간 [left, right]의 교집합의 최소 원소를 반환한다.
+		const int query(int left, int right, int node, int nodeLeft, int nodeRight) {
+			
+			// 두 구간이 겹치지 않으면 아주 큰 값을 반환한다. (교집합이 공집합인 경우)
+			if (right < nodeLeft || nodeRight < left) { return INT_MAX; }
 
-	int query(int left, int right) { return query(left, right, 1, 0, n - 1); }
+			// Node가 표현하는 범위 Array[nodeLeft, NodeRight]에 완전히 포함되는 경우
+			if (left <= nodeLeft && nodeRight <= right) { return this->rangeMin[node]; }
+
+			// 이 외의 모든 경우 (양쪽 구간을 나누어서 푼 뒤 결과를 합친다.)
+			const int mid = (nodeLeft + nodeRight) / 2;
+			return min(query(left, right, node * 2, nodeLeft, mid), query(left, right, node * 2 + 1, mid + 1, nodeRight));
+		} // 노드가 표현하는 구간이 찾고자하는 구간에 완전히 포함되거나 아예 겹쳐지지 않는 경우에는 탐색을 종료한다.
+
+	public :
+		const int query(int left, int right) { return this->query(left, right, 1, 0, length - 1); } // Time Complxity - log(n)
 } RMQ;
-
-vector<int> mVector;
-
-int main(void)
-{
-	int mNum1 = 0, mNum2 = 0;
-	scanf("%d %d", &mNum1, &mNum2);
-
-	for (int ii = 0, mTemp; ii < mNum1; ii++)
-	{
-		scanf("%d", &mTemp); mVector.push_back(mTemp);
-	}
-
-	RMQ mRMQ(mVector);
-
-	for (int ii = 0; ii < mNum2; ii++)
-	{
-		int mTemp[2] = { 0,0 };
-		scanf("%d %d", &mTemp[0], &mTemp[1]);
-
-		printf("%d\n", mRMQ.query(mTemp[0] - 1, mTemp[1] - 1));
-	}
-
-	return 0;
-}
